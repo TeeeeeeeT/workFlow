d383e35af0412c57c2769cc625ad0703a5f059e4
shinesheng <798650453@qq.com>
2023-10-10 16:49:21 +0800

    chore(模块): 修改eslint配置

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,3 +1,9 @@
 module.exports = {
-  extends: require.resolve("@umijs/max/eslint")
+  extends: require.resolve('@umijs/max/eslint'),
+  rules: {
+    eqeqeq: 'off',
+    'no-param-reassign': 'off',
+    'no-use-before-define': 'off',
+    '@typescript-eslint/no-use-before-define': 'off',
+  },
 };
