9182fbd7f5cb1e25cafd8571e5e599b9e42b3e3f
shinesheng <798650453@qq.com>
2023-11-23 11:37:24 +0800

    feat(模块): 流程模板列表

--- src/pages/workflow/scheme/list.tsx
+++ src/pages/workflow/scheme/list.tsx
@@ -1,74 +1,114 @@
 import React, { useEffect, useState } from 'react';
-import {
-    ProTable,
-} from '@ant-design/pro-components';
-import { Button, Divider, Drawer, message } from 'antd';
+import ListView from '@/common/listview';
+import Button from '@/component/button';
+import Modal from '@/component/dialog/modal';
 
-// import services from '@/services';
-// const { queryUserList } = services.UserController;
-
-import { schemeGetPageList } from '@/api/workflow/scheme';
-
-const Temp = () => {
-    const [createModalVisible, handleCreateModalVisible] = useState<boolean>(false);
-    const [updateModalVisible, handleUpdateModalVisible] = useState<boolean>(false);
-    const columns = [
-        {
-            title: '流程模板名称',
-            dataIndex: 'name',
-            key: 'name',
-        },
-        {
-            title: '编码',
-            dataIndex: 'code',
-            key: 'code',
-        },
+const Temp = (props: any) => {
+    const tabs = {
+        items: ['<div>122</div>', (d: any) => {
+            return (
+                <div>
+                    <a>修改</a>
+                </div>
+            );
+        }]
+    }
+    //查询框条件
+    const groups = [
+        [
+            {
+                span: '4',
+                type: 'Select',
+                label: '角色名称:',
+                name: 'roleid'
+            },
+            {
+                span: 4,
+                type: 'TextInput',
+                label: '用户名称:',
+                name: 'realName',
+                placeholder: '请输入用户名称'
+            },
+            {
+                span: 4,
+                type: 'TextInput',
+                label: '手机号:',
+                name: 'phoneNumber',
+                placeholder: '请输入手机号'
+            },
+            {
+                span: '4',
+                type: 'Select',
+                label: '是否启用:',
+                name: 'status',
+                options: [
+                    { name: '全部', value: '' },
+                    { name: '启用', value: '01' },
+                    { name: '禁用', value: '00' }
+                ]
+            }
+        ]
+    ];
+    //table列头
+    const tableStruct = [
+        { title: '编号', key: 'code' },
+        { title: '名称', key: 'name' },
         {
             title: '操作',
-            dataIndex: 'option',
-            key: 'option',
-            render: (_: any, record: any) => (
-                <>
-                    <a
-                    // onClick={() => {
-                    //     handleUpdateModalVisible(true);
-                    //     setStepFormValues(record);
-                    // }}
-                    >
-                        编辑
-                    </a>
-                    {/* <Divider type="vertical" /> */}
-                    {/* <a href="">订阅警报</a> */}
-                </>
-            ),
-        },
+            width: 160,
+            key: (d: any) => {
+                return (
+                    <div>
+                        <a>修改</a>
+                    </div>
+                );
+            }
+        }
     ];
+    let params: any = {};
+    for (let o in props.params) {
+        params[o] = props.params[o];
+    }
+    params.pageSize = params.pageSize || 10;
+    params.pageNo = params.pageNo || 1;
+
+    let operations: any = [
+        <Button text="新增" style="yellow" type="link" onClick={() => { setCreateModalVisible(true) }} />
+    ];
+
+    let [createModalVisible, setCreateModalVisible] = useState<boolean>(false);
+
     useEffect(() => {
     }, []);
 
-    return (
-        <ProTable
-            toolBarRender={() => [
-                <Button
-                    key="1"
-                    type="primary"
-                    onClick={() => handleCreateModalVisible(true)}
-                >
-                    新建
-                </Button>,
-            ]}
-            columns={columns}
-            request={async (params, sorter, filter) => {
-                const res: any = await schemeGetPageList({
-                    ...params
-                });
-                return {
-                    data: res?.data?.rows || [],
-                    success: res?.code == 200,
-                };
-            }}
+
+    return (<>
+        <ListView
+            url={'/workflow/schemeInfo/getPageList'}
+            method="get"
+            contentType="application/json"
+            params={params}
+            tableStruct={tableStruct}
+            groups={groups}
+            operations={operations}
         />
-    );
+        <Modal
+            visible={createModalVisible}
+            title='新增流程模板'
+            style={{
+                width: 500,
+                height: 500
+            }}
+            onCancel={() => { 
+                setCreateModalVisible(false) 
+            }}
+            onOk={() => { 
+                setCreateModalVisible(false) 
+            }}
+        >
+            <div>666</div>
+        </Modal>
+    </>);
 }
 
 export default Temp;
